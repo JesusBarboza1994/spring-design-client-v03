318dd22538ac804cdb57080cf1e5f320fdbb570c
Jesus Barboza <jbarz20@gmail.com>
2024-02-13 13:17:46 -0500

    FIX: comentar ruta /spring-type porq no tiene contenido

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Design from "./pages/Design/Design";
 import Design2 from "./pages/Design2/Design2";
-import SpringType from "./pages/SpringType";
+// import SpringType from "./pages/SpringType";
 import DesignInputData from "./pages/DesignInputData/DesignInputData";
 import TotalizedFormat from "./pages/TotalizedFormat/TotalizedFormat";
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>}/>
-          <Route path="/spring_type" element={<SpringType/>}/>
+          {/* <Route path="/spring-type" element={<SpringType/>}/> */}
           <Route path="/design_inputdata" element={<DesignInputData/>}/>
           <Route path="/prueba" element={<TotalizedFormat/>}/>
           <Route path="/design" element={<Design/>}/>
