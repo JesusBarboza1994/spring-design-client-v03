33162320fdcb15490526a55378ecc4fb3d976313
Jesus Barboza <jbarz20@gmail.com>
2024-02-26 15:49:45 -0500

    FIX: {

--- src/components/Design/ClientRequirements/ClientRequirements.js
+++ src/components/Design/ClientRequirements/ClientRequirements.js
@@ -1,114 +1,188 @@
-import { useState } from "react"
-import Input from "../../Input"
-import { Wrapper, Div } from "./styles"
-import TextArea from "./TextArea"
-import RequirementsTable from "./RequirementsTable"
-import TestTable from "./TestTable"
-import LoadConditionsTable from "./LoadConditionsTable"
+import { useState } from "react";
+import Input from "../../Input";
+import { Wrapper, Div } from "./styles";
+import TextArea from "./TextArea";
+import RequirementsTable from "./RequirementsTable";
+import TestTable from "./TestTable";
+import LoadConditionsTable from "./LoadConditionsTable";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-export function ClientRequirements({setCounter, counter}){
-    const [clientRequirements, setClientRequirements] = useState(JSON.parse(localStorage.getItem("ClientRequirements")) ||{
-      requirements:{
+export function ClientRequirements({ setCounter, counter }) {
+  const [clientRequirements, setClientRequirements] = useState(
+    JSON.parse(localStorage.getItem("ClientRequirements")) || {
+      requirements: {
         stiffnessVariation: "",
-        heightVariation:"",
-        stiffnessIndication:"",
-        heightIndication:""
+        heightVariation: "",
+        stiffnessIndication: "",
+        heightIndication: "",
       },
       otherRequirements: "",
       reason: "",
-      test40:"",
-      loadConditions:{
+      test40: "",
+      loadConditions: {
         personWeight: 80,
         roofWeight: 0,
         trunkWeight: 0,
-        peoplebyRow: [2, 3, 0, 0, 0]
+        peoplebyRow: [2, 3, 0, 0, 0],
       },
       observations: "",
-      bumpTest:[
+      bumpTest: [
         {
           del: 0,
           pos: 0,
           vel: 0,
-          comment: ""
+          comment: "",
         },
         {
           del: 0,
           pos: 0,
           vel: 0,
-          comment: ""
+          comment: "",
         },
         {
           del: 0,
           pos: 0,
           vel: 0,
-          comment: ""
+          comment: "",
         },
         {
           del: 0,
           pos: 0,
           vel: 0,
-          comment: ""
+          comment: "",
         },
         {
           del: 0,
           pos: 0,
           vel: 0,
-          comment: ""
-        }
+          comment: "",
+        },
       ],
-      signature:null
-    })
-
-    const handleChange = (e) =>{
-      setClientRequirements({...clientRequirements,	[e.target.id]: e.target.value })
-      let acc = 0
-      if(clientRequirements.otherRequirements !== "") acc += 1
-      if(clientRequirements.observations !== "") acc += 1
-      if(clientRequirements.reason !== "") acc += 1
-      if(clientRequirements.requirements.stiffnessIndication !== "" 
-        && clientRequirements.requirements.heightIndication !== ""
-        && clientRequirements.requirements.stiffnessVariation !== ""
-        && clientRequirements.requirements.heightVariation !== "") acc += 1
-
-      setCounter({...counter, ClientRequirements: {quantity: 4, counter: acc}})
-      localStorage.setItem("ClientRequirements", JSON.stringify({...clientRequirements, [e.target.id]: e.target.value }))
-      localStorage.setItem("counter", JSON.stringify({...counter, ClientRequirements: {quantity: 4, counter: acc}}))
+      signature: null,
     }
+  );
 
-    const handleImageChange = (event) => {
-      const file = event.target.files[0]; // Obtiene el primer archivo seleccionado
-      const reader = new FileReader(); // Crea un objeto FileReader
-  
-      // Maneja el evento load del FileReader
-      reader.onload = () => {
-        setClientRequirements({...clientRequirements, signature: reader.result});
-        localStorage.setItem('ClientRequirements', JSON.stringify({...clientRequirements, signature: reader.result}));
-      };
-  
-      // Lee el contenido del archivo como una URL de datos
-      reader.readAsDataURL(file);
+  const handleChange = (e) => {
+    setClientRequirements({
+      ...clientRequirements,
+      [e.target.id]: e.target.value,
+    });
+    let acc = 0;
+    if (clientRequirements.otherRequirements !== "") acc += 1;
+    if (clientRequirements.observations !== "") acc += 1;
+    if (clientRequirements.reason !== "") acc += 1;
+    if (
+      clientRequirements.requirements.stiffnessIndication !== "" &&
+      clientRequirements.requirements.heightIndication !== "" &&
+      clientRequirements.requirements.stiffnessVariation !== "" &&
+      clientRequirements.requirements.heightVariation !== ""
+    )
+      acc += 1;
+
+    setCounter({
+      ...counter,
+      ClientRequirements: { quantity: 4, counter: acc },
+    });
+    localStorage.setItem(
+      "ClientRequirements",
+      JSON.stringify({ ...clientRequirements, [e.target.id]: e.target.value })
+    );
+    localStorage.setItem(
+      "counter",
+      JSON.stringify({
+        ...counter,
+        ClientRequirements: { quantity: 4, counter: acc },
+      })
+    );
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0]; // Obtiene el primer archivo seleccionado
+    const reader = new FileReader(); // Crea un objeto FileReader
+
+    // Maneja el evento load del FileReader
+    reader.onload = () => {
+      setClientRequirements({
+        ...clientRequirements,
+        signature: reader.result,
+      });
+      localStorage.setItem(
+        "ClientRequirements",
+        JSON.stringify({ ...clientRequirements, signature: reader.result })
+      );
     };
-    
-    return(
-		<Wrapper>
-		 <Div>
-        <RequirementsTable clientRequirements={clientRequirements} setClientRequirements={setClientRequirements} counter={counter} setCounter={setCounter}/>
-        <LoadConditionsTable clientRequirements={clientRequirements} setClientRequirements={setClientRequirements}/>
-        <TextArea title="Otros requerimientos:" value={clientRequirements.otherRequirements} onChange={(e)=> handleChange(e)} id={"otherRequirements"}/>
-        <TextArea title="Observaciones:" value={clientRequirements.observations} onChange={(e)=> handleChange(e)} id={"observations"}/>
-        <TextArea title="Motivo de solicitud" value = {clientRequirements.reason} onChange={(e)=> handleChange(e)} id={"reason"}/>
-        <TestTable clientRequirements={clientRequirements} setClientRequirements={setClientRequirements}/>
-        <TextArea title="Prueba de freno brusco (40 km/h)" value={clientRequirements.test40} onChange={(e)=> handleChange(e)} id={"test40"}/>
+
+    // Lee el contenido del archivo como una URL de datos
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <Wrapper>
+      <Div>
+        <RequirementsTable
+          clientRequirements={clientRequirements}
+          setClientRequirements={setClientRequirements}
+          counter={counter}
+          setCounter={setCounter}
+        />
+        <LoadConditionsTable
+          clientRequirements={clientRequirements}
+          setClientRequirements={setClientRequirements}
+        />
+        <TextArea
+          title="Otros requerimientos:"
+          value={clientRequirements.otherRequirements}
+          onChange={(e) => handleChange(e)}
+          id={"otherRequirements"}
+        />
+        <TextArea
+          title="Observaciones:"
+          value={clientRequirements.observations}
+          onChange={(e) => handleChange(e)}
+          id={"observations"}
+        />
+        <TextArea
+          title="Motivo de solicitud"
+          value={clientRequirements.reason}
+          onChange={(e) => handleChange(e)}
+          id={"reason"}
+        />
+        <TestTable
+          clientRequirements={clientRequirements}
+          setClientRequirements={setClientRequirements}
+        />
+        <TextArea
+          title="Prueba de freno brusco (40 km/h)"
+          value={clientRequirements.test40}
+          onChange={(e) => handleChange(e)}
+          id={"test40"}
+        />
+        {clientRequirements.signature ? (
           <div>
-            <FaRegTrashAlt onClick={() => setClientRequirements({...clientRequirements, signature: null})} style={{cursor: 'pointer'}}/>
-            <img src={clientRequirements.signature} alt="Imagen seleccionada" style={{ maxWidth: '100%' }} />
+            <FaRegTrashAlt
+              onClick={() =>
+                setClientRequirements({
+                  ...clientRequirements,
+                  signature: null,
+                })
+              }
+              style={{ cursor: "pointer" }}
+            />
+            <img
+              src={clientRequirements.signature}
+              alt="Imagen seleccionada"
+              style={{ maxWidth: "100%" }}
+            />
           </div>
-           : 
-          <input type="file" onChange={handleImageChange} capture="user" accept="image/*"/>
-        }
-                        
+        ) : (
+          <input
+            type="file"
+            onChange={handleImageChange}
+            capture="user"
+            accept="image/*"
+          />
+        )}
       </Div>
     </Wrapper>
-    )
+  );
 }
